9cec7663e093d1de339870204d18e065655395ab
villysiu <villysiu@gmail.com>
2022-09-15 18:58:05 -0700

    devise JWT setup

--- api/app/controllers/games_controller.rb
+++ api/app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+    before_action :authenticate_user!
     def index
         games = Game.all
         render json: games, except: [:created_at, :updated_at]
